4d0bbbd01bc34d752b26aa656cf07977d5669132
suhit.patil <suhit.patil@globallogic.com>
2014-12-28 11:03:57 +0530

    Initial Commit

---
+++ PhotoLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoLibrary
+//
+//  Created by Suhit on 28/12/14.
+//  Copyright (c) 2014 com.suhit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
